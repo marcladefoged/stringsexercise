7fa768547a8081bb4e1f95802b848aec728aa0ab
Marc <marc489j@stud.kea.dk>
2021-08-30 00:59:34 +0200

    substring folder added

--- substring.js
+++ substring.js
@@ -3,4 +3,4 @@
 const fullname = "Albus Percival Wulfric Brian Dumbledore";
 const firstname = fullname.substring(0, 5);
 
-console.log(`First name is ${firstname}`);
+console.log(`First name is ${firtname}`);
