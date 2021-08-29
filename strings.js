cced8a69f6a952171be7ff7a9e1d1ef80fa8f335
Marc <marc489j@stud.kea.dk>
2021-08-30 00:31:55 +0200

    substring added

--- strings.js
+++ strings.js
@@ -12,5 +12,13 @@ const len = name.length;
 
 console.log(`${name} is ${len} characters long`);
 
-const letter = name[1];
+const letter = name[0];
 console.log(`The first letter of ${name} is ${letter}`);
+
+const str1 = "   There is     space here \n  ";
+
+console.log(str1);
+
+const str2 = str1.trim();
+
+console.log(str2);
---
+++ substring.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const fullname = "Albus Percival Wulfric Brian Dumbledore";
+const firstname = fullname.substring(0, 5);
+
+console.log(`First name is ${firstname}`);
