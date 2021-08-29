58cbd3908ea6606c0f68055defd9c5bd888f57d0
Marc <marc489j@stud.kea.dk>
2021-08-29 23:23:56 +0200

    first commit

---
+++ strings.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const name = "Peter";
+const drink = "Coca-cola";
+
+const text = `Hello ${name}, 
+would you like a ${drink}?`;
+
+console.log(text);
---
+++ template.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const name = "Marc";
+const animal = "dog";
+const petname = "Torben";
+
+const text = `My name is ${name}.
+I have a ${animal} called ${petname}.`;
+
+console.log(text);
